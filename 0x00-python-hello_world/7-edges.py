e287a6580ec98370ed519d8091acc69b57c6d75e
kawthar <kawtharikram2020@gmail.com>
2024-01-02 13:26:19 -0800

    python

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3, word_last_2, middle_word = word[:3], word[-2:], word[1:-1]
+print("First 3 letters:", word_first_3)
+print("Last 2 letters:", word_last_2)
+print("Middle word:", middle_word)
+
