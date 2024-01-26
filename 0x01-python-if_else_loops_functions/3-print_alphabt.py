71c1dec64e43306e99bb3488664097458cc8f67c
kawthar <kawtharikram2020@gmail.com>
2024-01-26 12:50:23 -0800

    python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for x in range(97, 123):
-    if chr(x) != 'q' and chr(x) != 'e':
-        print("{}".format(chr(x)), end="")
+for c in range(97, 123):
+    if c != 101 and c != 113:
+        print('{:c}'.format(c), end='')
